80bd61690beb4aeabe30d20de377c97c20382d23
zolon-dev <app@zolon.net>
2026-09-26 14:50:51 +0900

    出力処理途中

--- scripts/convert.py
+++ scripts/convert.py
@@ -4,16 +4,82 @@ import csv
 import sys
 from pathlib import Path
 
-
+# 品詞変換テーブル ZLex -> Google
+POS_MAP = {
+    "PROPER_NOUN": "固有名詞",
+    "NOUN": "名詞"
+}
 
 def convert_google(input_file: str, target_tag: str | None = None):
     print(f"{input_file} ... {target_tag}")
 
     input_path = Path(input_file)
 
+    print(f"input_path: {input_path}")
+
     if not input_path.exists():
         print(f"Error: ファイルが見つかりません: {input_path}")
         sys.exit(1)
+
+    # 出力先ディレクトリ(/dist)がなければ作成
+    output_dir = Path("dist")
+    output_dir.mkdir(exist_ok=True)
+
+    # 出力ファイル名
+    output_path = output_dir / f"{input_path.stem}_google.txt"
+
+    print(f"output_path: {output_path}")
+
+    # 出力件数
+    converted_count = 0
+
+    with open(input_path, "r", encoding="utf-8", newline="") as infile, \
+        open(output_path, "w", encoding="utf-8", newline="") as outfile:
+
+        reader = csv.reader(infile, delimiter="\t")
+        writer = csv.writer(
+            outfile,
+            delimiter="\t",
+            lineterminator="\n"
+        )
+
+        for row in reader:
+            # 空行スキップ
+            if not row:
+                continue
+            elif len(row) < 3: # 3個は必須なのでなかったらエラースキップ
+                print(f"必須項目が足りない {row}")
+                continue
+
+            print(f"{row}")
+            # 書いて無い項目は空文字にする
+            reading = row[0]
+            word = row[1]
+            pos = row[2]
+            comment = row[3] if len (row) >= 4 else "" # 任意
+            tags_raw = row[4] if len (row) >= 5 else "" # 任意
+
+            # タグフィルタリング
+            if target_tag:
+                # 元データタグを分解
+                tags = [t.strip() for t in tags_raw.split(",") if t.strip()] # ifは空文字防止
+
+                # 引数タグを分解
+                target_tags = [t.strip() for t in target_tag.split(",") if t.strip()]
+
+                # 引数のタグどれか1つが該当したらbreak
+                if not any(tag in tags for tag in target_tags):
+                    continue
+
+            # 品詞変換処理(不明なposは名詞扱い)
+            google_pos = POS_MAP.get(pos, "名詞")
+
+            # 書き込み
+
+
+
+
+
 
 
 
@@ -23,8 +89,10 @@ if __name__ == "__main__":
 
     if not args:
         print("使い方: python convert.py ***.tsv [タグ]")
+        print("複数タグ指定はOR検索: [タグ1,タグ2]")
         print("例1: python convert.py sample.tsv")
-        print("例2: python convert.py sample.tsv 役")
+        print("例2: python convert.py sample.tsv 役満")
+        print("例3: python convert.py sample.tsv 満貫,役満")
         sys.exit(1)
 
     input_file = args[0]
